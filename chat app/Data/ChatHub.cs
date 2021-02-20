907a5e933c7c6f2fa0a3582a29132859093c9b6d
zahra <zhram4459@gmail.com>
2021-02-20 18:36:13 +0200

    first Upload

---
+++ chat app/Data/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat_app.Data
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string sender ,string receiver, string msgTitle,string msgBody)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", sender, receiver, msgTitle, msgBody);
+        }
+        public async Task SendMessage2(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage2", user, message);
+        }
+    }
+}
---
+++ chat app/Data/NotificationMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat_app.Data
+{
+    public class NotificationMessage
+    {
+        public int MsgId { get; set; } = 0;
+        public string SenderName { get; set; } = "";
+        public string ReceiverName { get; set; } = "";
+        public string MsgTitle { get; set; } = "";
+        public string MsgBody { get; set; } = "";
+        public DateTime MsgDate { get; set; } = DateTime.Now;
+        public string MsgDateSt => this.MsgDate.ToString("dd-MMM-yyyy");
+
+
+    }
+}
